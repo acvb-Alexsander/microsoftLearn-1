9552962a4758c88a783a9bae872dcfdfaf81d678
acvb-Alexsander <acvb.developer@gmail.com>
2026-02-25 21:47:57 -0300

    csharp-if-elseif-else/7-summary#completion

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,6 +1,6 @@
 ﻿Random dice = new Random();
 
-int roll = dice.Next(1,7);
+/*int roll = dice.Next(1,7);
 
 int roll2 = dice.Next(101);
 
@@ -31,16 +31,22 @@ else if ((roll == roll2) && (roll2 == roll3))
     Console.WriteLine("No doubles rolled, no bonus.");
 }
 
-if (total >= 15)
+if (total >= 16)
 {
-    Console.WriteLine("You win!");
+    Console.WriteLine("You win a new car!");
 }
-
-if (total < 15)
+else if (total >= 10)
 {
-    Console.WriteLine("Sorry, you lose.");
-} 
-
+    Console.WriteLine("You win a new laptop!");
+}
+else if (total == 7)
+{
+    Console.WriteLine("You win a trip for two!");
+}
+else
+{
+    Console.WriteLine("You win a kitten!");
+}
 string message ="The quick brown fox jumps over the lazy dog.";
 
 bool result = message.Contains("dog");
@@ -57,4 +63,34 @@ else
 if (message.Contains("fox"))
 {
     Console.WriteLine("what does the fox say?");
+}
+*/
+
+Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+Console.WriteLine($"Your subscription days left {daysUntilExpiration}");
+if (daysUntilExpiration <= 10)
+{
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
+}
+else if (daysUntilExpiration <
+=5)
+{
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days. Renew now and save 10%!");
+    discountPercentage = 10;
+}
+else if (daysUntilExpiration == 1)
+{
+    Console.WriteLine($"Your subscription expires within a day.");
+    discountPercentage = 20;
+}
+else if (daysUntilExpiration == 0)
+{
+    Console.WriteLine($"Your subscription has expired.");
+}
+else
+{
+    Console.WriteLine($"Your subscription is Up to date. Enjoy!");
+    
 }
